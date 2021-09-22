2c28422152a6adfe84e9e1806db4162975794b72
Aditya shirsath <75840165+AdiShirsath@users.noreply.github.com>
2021-09-22 11:47:25 +0530

    updating script

--- src/train_word2vec.py
+++ src/train_word2vec.py
@@ -1,10 +1,9 @@
 from gensim.models import Word2Vec
-import logging
 from argparse import ArgumentParser
+import pickle
 
 def preparing_data(args):
     if args.data_path:
-        print("Creating dataset")
         with open(args.data_path, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             train_data = [line.strip().split() for line in lines]
@@ -14,12 +13,12 @@ def preparing_data(args):
 
 def train_model(args, train_data):
     # train model
-    print("training model...")
-    model = Word2Vec(train_data, size=args.size, window=args.window, min_count=args.min_count, workers=args.window)
-    
+    print(f"training word2vec with {args}")
+    model = Word2Vec(train_data, size=args.size, window=args.window, min_count=args.min_count, workers=args.workers, max_vocab_size=args.vocab_size, negative=args.negative)
     print("saving model")
     model.save("word2vec.bin")
     model.wv.save_word2vec_format('word2vec.txt')
+  
 
 def train():
     parser = ArgumentParser()
@@ -28,6 +27,9 @@ def train():
     parser.add_argument("--window", type=int, default=5, help="word2vec model's window")
     parser.add_argument("--min_count", type=int, default=10, help="word2vec model's min_count")
     parser.add_argument("--workers", type=int, default=4, help="word2vec model's window")
+    parser.add_argument("--vocab_size", type=int, default=None, help="word2vec vocab size")
+    parser.add_argument("--negative", type=int, default=5, help="negative samples")
+
     # create arg
     args= parser.parse_args()
     train_data = preparing_data(args)
